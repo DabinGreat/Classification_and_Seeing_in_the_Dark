708638bda3e102ef7497641df7d14e8f97cf7ccc
dabincheng <873601257@qq.com>
2020-05-04 14:48:42 +0800

    精细网络结构1.3

--- train_demo.py
+++ train_demo.py
@@ -54,7 +54,7 @@ def train():
 
         print(i_batch + 1, 'loss:{}\t'.format(loss.item()), 'train_acc:{}\n'.format(train_acc.getValue()),
               # 'pre:{}\t'.format(predictions.data), 'label:{}\n'.format(label),
-              'confidence :{}\t'.format(soft_out),
+              'confidence:{}\t'.format(soft_out),
               'clas:{}'.format(clas))
 
     return
